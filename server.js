a4e89effe99ca41e9117d08679d16201c5f95b00
mrdeepak125 <asus0gl8@outlook.com>
2024-06-24 10:52:17 +0530

    resolve

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URL, {
-  // useNewUrlParser: true,
-  // useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
